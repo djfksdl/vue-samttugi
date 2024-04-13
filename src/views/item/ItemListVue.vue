6bc2df4766de99dd1507d32b1d9e0351b117f44c
djfksdl <djfksdl@naver.com>
2024-04-13 14:32:32 +0900

    리스트 만드는중

--- src/views/item/ItemListVue.vue
+++ src/views/item/ItemListVue.vue
@@ -5,7 +5,36 @@
             <AppHeader/>
             <!-- 내용 -->
             <div class="ItemListContainer">
-                <div class="ItemListInner"></div>
+                <div class="ItemListInner">
+                    <!-- 경로 -->
+                    <div class="channelSrc">
+                        <router-link to="">홈</router-link>
+                        <router-link to="">라면/컵누들/곤누들</router-link>
+                    </div>
+                    <!-- 타이틀 -->
+                    <h2>라면/컵누들/곤누들</h2>
+                    <!-- 상품리스트 -->
+                    <div class="ItemListSection">
+                        <!-- 탭메뉴 -->
+                        <ul class="ItemTapMenu">
+                            <li><router-link to="">전체</router-link></li>
+                            <li><router-link to="">봉지라면</router-link></li>
+                            <li><router-link to="">컵라면</router-link></li>
+                            <li><router-link to="">컵누들</router-link></li>
+                            <li><router-link to="">라면스프</router-link></li>
+                            <li><router-link to="">곤누들</router-link></li>
+                            <li><router-link to="">잡채</router-link></li>
+                            <li><router-link to="">사리면</router-link></li>
+                        </ul>
+                        <!-- 상품리스트나열 -->
+                        <div class="ItemContentBox">
+                            <div class="ItemContentImg">
+                                <img src="@/assets/images/JinRamenSpicy.jpg">
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
             </div>
             <!-- 푸터 -->
             <AppFooter/>
