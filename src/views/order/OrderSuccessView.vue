f312c10d8a6ea312404803ed75b93824e6373965
rhs0402 <rhs0402@naver.com>
2024-04-17 20:50:08 +0900

    류하석

---
+++ src/views/order/OrderSuccessView.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="wrap">
+        <!-- 헤더 -->
+        <AppHeader />
+
+        <!-- 푸터 -->
+        <AppFooter />
+    </div>
+</template>
+<script>
+import AppHeader from '@/components/AppHeader.vue'
+import AppFooter from '@/components/AppFooter.vue'
+
+export default {
+    name: "OrderSuccessView",
+    components: {
+        AppHeader,
+        AppFooter
+    },
+    data() {
+        return {};
+    },
+    methods: {},
+    created() { }
+};
+</script>
