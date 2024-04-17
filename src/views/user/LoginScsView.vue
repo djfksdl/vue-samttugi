faa959b222860eeaff8f7de564e5db6d62c72796
ds-2024 <yoonds714@nate.com>
2024-04-17 11:24:13 +0900

    로그인

--- src/views/user/LoginScsView.vue
+++ src/views/user/LoginScsView.vue
@@ -1,33 +1,60 @@
 <template>
-    <AppHeader />
-    <!-- //header -->
-    <div class="success-container">
-      <span id="name">이연수님,</span> 
-      <span id="welcome"> 환영합니다!</span>
-        <p id="now">삼뚜기의 모든 것을 지금부터 만나보세요</p>
-        <img class="new" src="@/assets/images/new-product.png" alt="신상">
-    
-      <router-link to="/" id="startBtn">삼뚜기몰 시작하기</router-link>
-    </div>
-    <AppFooter />
-    <!--//footer-->
-  </template>
-  
-  <script>
-  import "@/assets/css/user/loginScs.css"
-  import AppFooter from '@/components/AppFooter.vue';
-  import AppHeader from '@/components/AppHeader.vue';
-  export default {
-      name: "LoginScsView",
-      components: {
-          AppFooter,
-          AppHeader
-      },
-      data() {
-          return {};
-      },
-      methods: {},
-      created() { }
-  };
-  </script>
-  
+  <AppHeader />
+  <!-- //header -->
+  <div class="success-container">
+    <span id="name">{{ usersVo.userName }}님,</span>
+    <span id="welcome"> 환영합니다!</span>
+    <p id="now">삼뚜기의 모든 것을 지금부터 만나보세요</p>
+    <img class="new" src="@/assets/images/new-product.png" alt="신상">
+
+    <router-link to="/" id="startBtn">삼뚜기몰 시작하기</router-link>
+  </div>
+  <AppFooter />
+  <!--//footer-->
+</template>
+
+<script>
+import axios from 'axios';
+import "@/assets/css/user/loginScs.css"
+import AppFooter from '@/components/AppFooter.vue';
+import AppHeader from '@/components/AppHeader.vue';
+export default {
+  name: "LoginScsView",
+  components: {
+    AppFooter,
+    AppHeader
+  },
+  data() {
+    return {
+      
+    };
+  },
+  methods: {
+    // 사용자 정보를 서버로부터 받아오는 메소드
+    getUserName() {
+      console.log("회원이름");
+      console.log(this.userName);
+      axios({
+        method: 'get',
+        url: 'http://localhost:9009/api/user/loginsuccess',
+        headers: {
+          "Content-Type": "application/json; charset=utf-8",
+          Authorization: "Bearer " + this.$store.state.token
+        },
+        responseType: "json"
+      })
+        .then(response => {
+          console.log(response.data);
+          // 서버로부터 받아온 사용자 정보에서 이름값을 추출
+          const userName = response.data.userName;
+          // 추출한 이름값을 화면에 표시
+          this.userName = userName;
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    }
+  },
+  created() { }
+};
+</script>
--- src/views/user/LoginView.vue
+++ src/views/user/LoginView.vue
@@ -13,12 +13,13 @@
                 </div>
                 <div class="login-form">
                     <form v-on:submit.prevent="login" method="post">
-                        <input type="text" placeholder="아이디">
-                        <input type="password" placeholder="비밀번호">
+                        <input type="text" placeholder="아이디" v-model="usersVo.id">
+                        <input type="password" placeholder="비밀번호" v-model="usersVo.password">
                         <button type="submit" class="loginBtn">로그인</button>
                         <router-link to="/loginform" class="signupBtn">회원가입</router-link>
                     </form>
                 </div>
+                <!-- //login-form -->
             </div>
         </div>
         <AppFooter />
@@ -28,6 +29,7 @@
 
 
 <script>
+import axios from 'axios';
 import "@/assets/css/user/login.css"
 import AppFooter from '@/components/AppFooter.vue';
 import AppHeader from '@/components/AppHeader.vue';
@@ -38,10 +40,59 @@ export default {
         AppHeader
     },
     data() {
-        return {};
+        return {
+            usersVo: {
+                id: "",
+                password: ""
+            }
+        };
     },
-    methods: {},
-    created() { }
+    methods: {
+        login(){
+            console.log("login");
+            console.log(this.usersVo);
+            axios({
+                method: 'post', // put, post, delete
+                url: 'http://localhost:9009/api/user/login',
+                headers: { "Content-Type": "application/json; charset=utf-8" }, //전송타입
+                //params: usersVo, //get방식 파라미터로 값이 전달
+                data: this.usersVo, //put, post, delete 방식 자동으로 JSON으로 변환 전달
+                responseType: 'json' //수신타입
+            }).then(response => {
+                console.log(response.data); //수신데이타 authUser
+                
+                if(response.data.result == "success"){
+                    //로그인 사용자 정보
+                    let authUser = response.data.apiData;
+
+                    //token은 응답문서의 헤더에 있음 
+                    console.log(response.headers.authorization.split(" ")[1]);
+                    //"Authorization Bearer dajofddjaoadfklh.adfafa"
+                    const token = response.headers.authorization.split(" ")[1];
+
+                    //vuex 저장
+                    this.$store.commit("setAuthUser", authUser);
+                    this.$store.commit("setToken", token);
+
+                    console.log(authUser);
+                    console.log(token);
+
+                    this.$router.push("/loginsuccess");
+                } else {
+                    console.log(response.data.message);
+                    alert("아이디 패스워드를 확인하세요.");
+                }
+
+            }).catch(error => {
+                console.log(error);
+            });
+        }
+        
+        /* login */
+    },
+    created() { 
+       
+    }
 };
 </script>
 
