34dadfaf628a8c23e50710f0434a83f8988fa4d2
djfksdl <djfksdl@naver.com>
2024-04-17 12:11:34 +0900

    스토리지 추가

--- src/store/storage.js
+++ src/store/storage.js
@@ -1,15 +1,31 @@
 import { createStore } from 'vuex';
+import createPersistedState from 'vuex-persistedstate';
 export default createStore({
     state() {
         return {
             apiBaseUrl: "http://localhost:9009",
             // apiBaseUrl: "http://13.125.251.10:9009",
+            authUser: null,
+            token: null
+
         };
     },
     mutations: {
-
-
+        setAuthUser(state, payload) {
+            state.authUser = payload;
+        },
+        setAuthName(state, payload) {
+            state.authUser.name = payload;
+        },
+        setToken(state, payload) {
+            state.token = payload;
+        },
+        
     },
     plugins: [
+        createPersistedState({
+            paths: ['authUser', 'token']
+        })
     ]
+
 });
