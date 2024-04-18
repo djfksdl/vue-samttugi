3992d7e5f9177df1543e0356cb01d3747b75b08c
rhs0402 <rhs0402@naver.com>
2024-04-18 18:46:22 +0900

    류하석

--- src/views/order/OrderView.vue
+++ src/views/order/OrderView.vue
@@ -2,7 +2,7 @@
     <div class="wrap">
 
         <!-- 헤더 -->
-        <AppHeader/>
+        <AppHeader />
 
         <div class="inner">
             <div class="location">
@@ -11,11 +11,11 @@
                     <li>주문결제</li>
                 </ul>
             </div>
-    
+
             <div class="title">
                 <h1>주문결제</h1>
             </div>
-    
+
             <div class="aSide">
                 <div class="order-product">
                     <div class="sub-title">
@@ -24,7 +24,7 @@
                             <span class="op-count"></span>
                         </div>
                     </div>
-    
+
                     <div class="productBox" v-bind:key="i" v-for="(dbcartVo, i) in orderList">
                         <img src="" alt="">
                         <p class="pName">{{ dbcartVo.productName }}</p>
@@ -32,13 +32,13 @@
                         <b class="p-charge">{{ (dbcartVo.price * dbcartVo.cCount).toLocaleString('ko-KR') }}</b>
                     </div>
                 </div>
-    
+
                 <div class="order-info">
                     <div class="sub-title">
                         <span>주문자 정보</span>
                         <span>주문자 정보는 <b>마이페이지 > 회원정보 변경</b>에서 변경하실 수 있습니다.</span>
                     </div>
-    
+
                     <div class="flex-box">
                         <div>
                             <p>보내는 분</p>
@@ -46,26 +46,26 @@
                             <p>이메일</p>
                         </div>
                         <div>
-                            <p>{{userInfo.userName}}</p>
-                            <p>{{userInfo.hp}}</p>
-                            <p>{{userInfo.email}}</p>
+                            <p>{{ userInfo.userName }}</p>
+                            <p>{{ userInfo.hp }}</p>
+                            <p>{{ userInfo.email }}</p>
                         </div>
                     </div>
                 </div>
-    
+
                 <div class="delivery-info">
                     <div class="sub-title">
                         <span>배송지 정보</span>
                         <button>배송지등록</button>
                     </div>
-    
+
                     <div class="flex-box">
                         <div>
                             <p>배송지</p>
                             <p>배송 요청사항</p>
                         </div>
                         <div>
-                            <p>{{userInfo.address}}</p>
+                            <p>{{ userInfo.address }}</p>
                             <select v-model="orderVo.request" name="request" id="">
                                 <option disabled>배송 요청사항</option>
                                 <option>부재시 경비실에 맡겨주세요.</option>
@@ -74,22 +74,22 @@
                             </select>
                         </div>
                     </div>
-    
+
                 </div>
-    
+
                 <div class="payment-box">
                     <div class="sub-title">
                         <span>결제수단</span>
-    
+
                         <div>
                             <input type="checkbox" name="" id="">
                             <label for="">선택한 결제수단 저장</label>
                         </div>
                     </div>
-    
+
                     <div class="payment-select">
                         <p>결제수단 선택</p>
-    
+
                         <div class="pay-icon">
                             <div class="pay-select credit" v-on:click="btnActive">신용카드</div>
                             <div class="pay-select kakao" v-on:click="btnActive">kakao</div>
@@ -101,7 +101,7 @@
                         <b>· 최초 결제 금액 100원 이상일 때 승인 처리가 가능해요.</b>
                     </div>
                 </div>
-    
+
                 <div class="agreement">
                     <div class="sub-title"></div>
                     <div>
@@ -121,17 +121,19 @@
                         <label for="private-agree"><b>(필수) </b>개인정보 수집, 이용에 관한 동의</label>
                     </div>
                 </div>
-    
+
                 <ul class="aSide-last-text">
                     <li>· 미성년자가 결제 시 법정대리인이 그 거래를 취소할 수 있습니다.</li>
                     <li>· 주문 취소 시 결제하신 수단으로만 환불됩니다.</li>
                 </ul>
-    
-                <button v-if="this.orderPrice < 30000" class="orderBtn" type="submit"  v-on:click="goToSuccess">{{ (this.orderPrice+3000).toLocaleString('ko-KR') }}원 주문하기</button>
-                <button v-else class="orderBtn" type="submit"  v-on:click="goToSuccess">{{ this.orderPrice.toLocaleString('ko-KR') }}원 주문하기</button>
-    
+
+                <button v-if="this.orderPrice < 30000" class="orderBtn" type="submit" v-on:click="goToSuccess">{{
+                        (this.orderPrice + 3000).toLocaleString('ko-KR') }}원 주문하기</button>
+                <button v-else class="orderBtn" type="submit" v-on:click="goToSuccess">{{
+                        this.orderPrice.toLocaleString('ko-KR') }}원 주문하기</button>
+
             </div><!-- //aSide -->
-    
+
             <div class="bSide">
                 <div class="b-top">
                     <h2>상품금액</h2>
@@ -146,7 +148,7 @@
                         </div>
                         <div class="final-charge">
                             <b>최종 결제금액</b>
-                            <b>{{ (this.orderPrice+3000).toLocaleString('ko-KR') }}원</b>
+                            <b>{{ (this.orderPrice + 3000).toLocaleString('ko-KR') }}원</b>
                         </div>
                     </div>
                     <div class="ifBox" v-else>
@@ -167,15 +169,17 @@
                         </ul>
                     </div>
                 </div><!-- //b-top -->
-    
-                <button v-if="this.orderPrice < 30000" class="orderBtn" type="submit" v-on:click="goToSuccess">{{ (this.orderPrice+3000).toLocaleString('ko-KR') }}원 주문하기</button>
-                <button v-else class="orderBtn" type="submit" v-on:click="goToSuccess">{{ this.orderPrice.toLocaleString('ko-KR') }}원 주문하기</button>
-    
+
+                <button v-if="this.orderPrice < 30000" class="orderBtn" type="submit" v-on:click="goToSuccess">{{
+                        (this.orderPrice + 3000).toLocaleString('ko-KR') }}원 주문하기</button>
+                <button v-else class="orderBtn" type="submit" v-on:click="goToSuccess">{{
+                        this.orderPrice.toLocaleString('ko-KR') }}원 주문하기</button>
+
             </div><!-- //bSid -->
         </div><!-- //inner -->
 
         <!-- 푸터 -->
-        <AppFooter/>
+        <AppFooter />
 
     </div><!-- //wrap -->
 </template>
@@ -203,7 +207,8 @@ export default {
                 address: ""
             },
             orderPrice: "",
-            orderVo:{
+            orderVo: {
+                userNo: this.$store.state.authUser.userNo,
                 payment: "",
                 request: ""
             },
@@ -223,21 +228,21 @@ export default {
             let credit = document.querySelector('.credit');
             let kakao = document.querySelector('.kakao');
             let naver = document.querySelector('.naver');
-            let payco =  document.querySelector('.payco');
+            let payco = document.querySelector('.payco');
             let account = document.querySelector('.acount');
             let live = document.querySelector('.live');
 
-            if(event.target == credit) {
+            if (event.target == credit) {
                 this.orderVo.payment = "신용카드";
             } else if (event.target == kakao) {
                 this.orderVo.payment = "카카오페이";
-            } else if(event.target == naver) {
+            } else if (event.target == naver) {
                 this.orderVo.payment = "네이버페이";
-            } else if(event.target == payco) {
+            } else if (event.target == payco) {
                 this.orderVo.payment = "페이코";
-            } else if(event.target == account) {
+            } else if (event.target == account) {
                 this.orderVo.payment = "가상계좌";
-            } else if(event.target == live) {
+            } else if (event.target == live) {
                 this.orderVo.payment = "실시간계좌이체";
             }
 
@@ -258,23 +263,23 @@ export default {
         },
         // 전체동의
         allAgree() {
-            if(document.querySelector('#all-agree').checked == true) {
-                for(let i=0; i<3; i++) {
-                    document.getElementsByName("checkBox")[i].checked=true;
+            if (document.querySelector('#all-agree').checked == true) {
+                for (let i = 0; i < 3; i++) {
+                    document.getElementsByName("checkBox")[i].checked = true;
                 }
             } else {
-                for(let i=0; i<3; i++) {
-                    document.getElementsByName("checkBox")[i].checked=false;
+                for (let i = 0; i < 3; i++) {
+                    document.getElementsByName("checkBox")[i].checked = false;
                 }
             }
         },
         // 동의사항 체크하면 전체동의도 같이 체크
-        rAllAgree(){
+        rAllAgree() {
             let payAgree = document.querySelector('#pay-agree');
             let useAgree = document.querySelector('#use-agree');
             let privateAgree = document.querySelector('#private-agree');
 
-            if(payAgree.checked == true && useAgree.checked == true && privateAgree.checked == true) {
+            if (payAgree.checked == true && useAgree.checked == true && privateAgree.checked == true) {
                 document.querySelector('#all-agree').checked = true;
             } else {
                 document.querySelector('#all-agree').checked = false;
@@ -288,12 +293,11 @@ export default {
                 method: 'get', // put, post, delete
                 url: 'http://localhost:9009/api/order/list',
                 headers: { "Content-Type": "application/json; charset=utf-8" }, //전송타입
-                params: {no: this.$store.state.authUser.userNo}, //get방식 파라미터로 값이 전달
+                params: { no: this.$store.state.authUser.userNo }, //get방식 파라미터로 값이 전달
                 //data: cList, //put, post, delete 방식 자동으로 JSON으로 변환 전달
                 responseType: 'json' //수신타입
             }).then(response => {
                 console.log(response); //수신데이타
-                console.log(this.$store.state.authUser.userNo);
                 this.orderList = response.data.apiData;
 
                 this.calculate();
@@ -303,21 +307,19 @@ export default {
             });
         },
         // 주문자정보 가져오기
-        getUserInfo(){
+        getUserInfo() {
             console.log("유저정보 가져오기");
 
             axios({
                 method: 'get', // put, post, delete
                 url: 'http://localhost:9009/api/order/userinfo',
                 headers: { "Content-Type": "application/json; charset=utf-8" }, //전송타입
-                params: {no: this.$store.state.authUser.userNo}, //get방식 파라미터로 값이 전달
+                params: { no: this.$store.state.authUser.userNo }, //get방식 파라미터로 값이 전달
                 //data: cList, //put, post, delete 방식 자동으로 JSON으로 변환 전달
                 responseType: 'json' //수신타입
             }).then(response => {
                 console.log(response); //수신데이타
-                console.log(this.$store.state.authUser.userNo);
                 this.userInfo = response.data.apiData;
-                console.log(this.userInfo);
 
             }).catch(error => {
                 console.log(error);
@@ -327,38 +329,56 @@ export default {
         calculate() {
             let orderPrice = 0;
             for (let i = 0; i < this.orderList.length; i++) {
-                orderPrice += (this.orderList[i].price*this.orderList[i].cCount);
+                orderPrice += (this.orderList[i].price * this.orderList[i].cCount);
             }
             this.orderPrice = orderPrice;
         },
         // 주문하기
         goToSuccess() {
-            
+
+            let orderInfoVo ={
+                orderVo: this.orderVo,
+                orderList: this.orderList
+            }
+
+            console.log(orderInfoVo)
+
+
             let payAgree = document.querySelector('#pay-agree');
             let useAgree = document.querySelector('#use-agree');
             let privateAgree = document.querySelector('#private-agree');
-            
+
             // 동의사항 체크 확인
-            if(payAgree.checked == false || useAgree.checked == false || privateAgree.checked == false) {
+            if (payAgree.checked == false || useAgree.checked == false || privateAgree.checked == false) {
                 alert("필수 동의사항을 체크해 주세요.");
 
             } else {
                 // 서버 통신 작성
-                console.log(this.orderVo);
-
                 //orderVo를 axios --> orders db에 저장
-                
-                //orderListVo를 axios --> orderList db에 저장
+                axios({
+                    method: 'post', // put, post, delete
+                    url: 'http://localhost:9009/api/order/orders',
+                    headers: { "Content-Type": "application/json; charset=utf-8" }, //전송타입
+                    params: { no: this.$store.state.authUser.userNo }, //get방식 파라미터로 값이 전달
+                    data: orderInfoVo, //put, post, delete 방식 자동으로 JSON으로 변환 전달
+                    responseType: 'json' //수신타입
+                }).then(response => {
+                    console.log(response); //수신데이타
 
-                // 주문 성공 화면으로 이동
-                // this.$router.push("/order/success");
+                    //cart에서 정보 삭제
+
+                    // 주문 성공 화면으로 이동
+                    // this.$router.push("/order/success");
+                }).catch(error => {
+                    console.log(error);
+                });
             }
-        }
+        },
     },
-    created(){
+    created() {
         this.getOrderList();
         this.getUserInfo();
-        
+
     }
 };
 </script>
