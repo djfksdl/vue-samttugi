fc2d44d1ef065a57c65611ea6041f37f920502d6
ds-2024 <yoonds714@nate.com>
2024-04-19 12:10:10 +0900

    DS주문완료이미지추가

--- src/views/order/OrderSuccessView.vue
+++ src/views/order/OrderSuccessView.vue
@@ -6,6 +6,8 @@
         <div class="successBox">
             <p><b>삼뚜기몰</b>을 이용해 주셔서 감사합니다.</p>
             <h1>주문이 성공적으로 완료되었습니다.</h1>
+            <img class="del" src="@/assets/images/del.png" alt="배송">
+            <h2>고객님의 소중한 상품을 안전하게 배송해 드리겠습니다.</h2>
             <button class="orderBtn" v-on:click="goToHome">홈으로</button>
         </div>
 
